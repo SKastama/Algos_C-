3b6ebfb56020bd6e4aac09412c3189f92eb52a32
Sarah Kastama <sarahkastama@gmail.com>
2021-09-28 10:05:26 -0700

    commit

---
+++ D2.js
@@ -0,0 +1,88 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    seedFromArr(vals) {
+        for (const item of vals) {
+            this.insertAtBack(item);
+        }
+        return this;
+    }
+
+    toArr() {
+        const arr = [];
+        let runner = this.head;
+
+        while (runner) {
+            arr.push(runner.data);
+            runner = runner.next;
+        }
+        return arr;
+    }
+
+    isEmpty() {
+        return this.head == null;
+    }
+
+    insertAtBack(data) {
+        const newNode = new Node(data);
+        if(this.isEmpty()) {
+            this.head = newNode;
+            return this;
+        }
+        let runner = this.head;
+        while(runner.next) {
+            runner = runner.next;
+        }
+        runner.next = newNode;
+        return this;
+    }
+
+    /**
+     * Creates a new node with the given data and inserts that node at the front
+     * of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} data The data for the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtFront(data) {
+        let newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = new Node(data);
+        return this;
+    }
+
+    /**
+     * Removes the first node of this list, and return the removed node.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {any} The data from the removed node.
+     */
+    removeHead() {
+        let oldHead = this.head;
+        this.head = this.head.next;
+        oldHead.next = null;
+    }
+
+    // EXTRA
+    /**
+     * Calculates the average of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {number|NaN} The average of the node's data.
+     */
+    average() {}
+
+}
+
+const insertFronttest = new SinglyLinkedList();
+console.log(insertFronttest.insertAtFront(7));
